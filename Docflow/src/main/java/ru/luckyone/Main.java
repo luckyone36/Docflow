d0e70b39972a0bb3ab7655eb48b18d44f3289eba
Lucky One <lu.on.git@yandex.ru>
2022-06-27 20:02:44 +0300

    into second task branch

--- Docflow/src/main/java/ru/luckyone/Main.java
+++ Docflow/src/main/java/ru/luckyone/Main.java
@@ -8,7 +8,7 @@ import ru.luckyone.entities.documents.Document;
 import ru.luckyone.exception.DocumentExistsException;
 import ru.luckyone.utils.DocumentsPrinter;
 /**
- *Программа по документообороту
+ *Программа по документообороту 20-00
  *
  * @author altair
  *
